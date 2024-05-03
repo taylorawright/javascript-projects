987f73fb18016556852109ce864811d1e733b2be
taylorawright <taylor.angeli@ymail.com>
2024-05-02 19:54:16 -0500

    class 2 shuttle launch studio

--- booleans-and-conditionals/studio/data-variables-conditionals.js
+++ booleans-and-conditionals/studio/data-variables-conditionals.js
@@ -13,3 +13,41 @@
 // add logic below to verify the weather status is clear
 
 // Verify shuttle launch can proceed based on above conditions
+let date = "Monday 2019-03-18"
+let time = "10:05:34 AM"
+let astronautCount = 7
+let astronautStatus = "ready"
+let averageAstronautMassKg = 80.7
+let crewMassKg = astronautCount * averageAstronautMassKg
+let fuelMassKg = 760000
+let shuttleMassKg = 74842.31
+let totalMassKg = crewMassKg + fuelMassKg + shuttleMassKg
+let maximumMassLimit = 850000
+let fuelTempCelsius = -225
+let minimumFuelTemp = -300
+let maximumFuelTemp = -150
+let fuelLevel = '100%' 
+let weatherStatus = 'clear'
+let preparedForLiftOff = true
+
+if ((astronautCount <= 7) && (astronautStatus === "ready") &&
+    (totalMassKg < maximumMassLimit) &&
+    (fuelTempCelsius >= -300 || fuelTempCelsius <= -150) &&
+    (fuelLevel === '100%') && (weatherStatus = 'clear')) {
+    console.log("All systems are a go! Initiating space shuttle launch sequence.");
+    console.log("---------------------------------------------------------------");
+    console.log("Date:", date);
+    console.log("Time:", time);
+    console.log("Astronaut Count:", astronautCount);
+    console.log("Crew Mass:", crewMassKg);
+    console.log("Fuel Mass:", fuelMassKg);
+    console.log("Shuttle Mass:", shuttleMassKg);
+    console.log("Total Mass:", totalMassKg);
+    console.log("Fuel Temperature:", fuelTempCelsius, "°C");
+    console.log("Weather Status:", weatherStatus);
+    console.log("--------------------------------------------------------------");
+    console.log("Have a safe trip astronauts!");
+
+} else {
+    console.log("Unsafe conditions! Launch aborted.");
+    }
